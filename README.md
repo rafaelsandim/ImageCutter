57df5a4aad1e9f2c575fff8387f70a55115a9d9d
rafaelsandim <rjsandim@gmail.com>
2014-04-06 04:02:01 -0400

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 ImageCutter
 ===========
+
+Fistly, clone or make download of ImageCutter on your Plugin folder, normally: app\Plugin 
+
+
+On app\Config\bootstrap.php add the follow line:
+
+```php
+  CakePlugin::load('ImageCutter');
+```
+
+Every View that you'll need to use ImageCutter you should put on view Controller the follow line:
+
+```php
+  public $helpers = array('ImageCutter.ImageCutter');
+```
+
+### How to use ###
+
+
+On view (app/View/\*/*.ctp), you can use ImageCutter Helper according next lines: 
+
+```php
+
+// PATH = file location (ex: './photos/car.png');
+// width = width value (ex: 100);
+// height = height value (ex: 200;
+<img src="<?=$this->ImageCutter->url('PATH', width, height)?>">
+
+// If you want to same dimension to several photos you can use setDimensions() to define a width and height
+<? $this->ImageCutter->setDimensions(300, 300); ?>
+<img src="<?=$this->ImageCutter->url('PATH_1')?>">
+<img src="<?=$this->ImageCutter->url('PATH_2')?>">
+<img src="<?=$this->ImageCutter->url('PATH_3')?>">
+
+// The default crop type is outside, however you can change it.
+// if you have setted dimensions with setDimensions(), just use:
+<img src="<?=$this->ImageCutter->url('PATH_1', 'outside')?>"> // is not necessary because outside is default.
+<img src="<?=$this->ImageCutter->url('PATH_2', 'inside')?>">
+<img src="<?=$this->ImageCutter->url('PATH_3', 'fill')?>">
+
+// If you don't have setDimensions() setted or want another dimension for a specific img tag
+// Is not necessary because outside is default.
+<img src="<?=$this->ImageCutter->url('PATH_1', 100, 400, 'outside')?>">
+// So
+<img src="<?=$this->ImageCutter->url('PATH_1', 100, 400)?>">
+<img src="<?=$this->ImageCutter->url('PATH_2', 155, 233, 'inside')?>">
+<img src="<?=$this->ImageCutter->url('PATH_3', 112, 333, 'fill')?>">
+
+
+```
---
+++ View/Helper/ImageCutterHelper.php
@@ -0,0 +1,68 @@
+<?php
+
+App::uses('AppHelper', 'View/Helper');
+App::import('Vendor', 'ImageCutter.WideImage/WideImage');
+
+class ImageCutterHelper extends AppHelper {
+    
+    private $width;
+    private $height;
+    private $crop = 'outside';
+
+    private function __param($parameter) {
+
+        $numberOfParameters = count($parameter);
+        $url = null;
+        $width = $this->width;
+        $height = $this->height;
+        $crop = $this->crop;
+
+        if ($numberOfParameters == 2) {
+            $crop = $parameter[1];
+        } else if ($numberOfParameters >= 3) {
+            $width = $parameter[1];
+            $height = $parameter[2];
+            if ($numberOfParameters == 4) {
+                $crop = $parameter[3];
+            }
+        }
+        $url = $parameter[0];
+
+        return array($url, $width, $height, $crop);
+    }
+
+    // When you want to use the same crop to a lot of images, 
+    //you can use setCrop and define a default crop, if you not 
+    //use and don't pass any parameter the default crop will be 'outside'.
+    public function setCrop($crop) {
+        $this->crop = $crop;
+    }
+
+    // When you want to use the same dimensions to a lot of images, 
+    //you can use setDimensions and define a default width and height.
+    public function setDimensions($width=150, $height=150) {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    //returns thumbs path if it exists, else generate a new thumb according parameters.
+    public function url($url=null, $full=false) {
+        list($url, $width, $height, $crop) = $this->__param(func_get_args());
+
+    	$file = pathinfo($url);
+    	$extension = $file['extension'];
+    	$dir = $file['dirname'];
+    	$name = $file['filename'];
+
+    	$outputFilePath = $dir.'/'.$name.'_'.$width.'x'.$height.'_'.$crop.'.'.$extension;
+    
+    	if (!file_exists($outputFilePath)) {
+    		$image = WideImage::load($url);
+    		$image = $image->resize($width, $height, $crop, 'any')->crop('center', 'center', $width, $height);
+    		$quality = strcmp($extension, 'png') ? 100 : 9;
+    		$image->saveToFile($outputFilePath, $quality);
+    	}
+
+    	return $outputFilePath;
+    }
+}
